3bc2c1088232286f23177d87f0f4803538ef0af3
HenriqueLDSa <rique.ldsa@gmail.com>
2024-05-02 18:35:15 -0400

    Created stopwatch activity with basic layout and class functionality

--- app/src/main/java/com/application/clickcounter/CountdownTimer.java
+++ app/src/main/java/com/application/clickcounter/CountdownTimer.java
@@ -1,4 +1,26 @@
 package com.application.clickcounter;
 
-public class CountdownTimer {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CountdownTimer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_countdown_timer);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_countdown_timer), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+    }
 }
--- app/src/main/java/com/application/clickcounter/Stopwatch.java
+++ app/src/main/java/com/application/clickcounter/Stopwatch.java
@@ -3,25 +3,22 @@ package com.application.clickcounter;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
-public class MainActivity extends AppCompatActivity {
+public class Stopwatch extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_stopwatch);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.stopwatch_activity), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
     }
 }
